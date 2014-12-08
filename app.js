063642aa8d8e7a8279a7696e04f4ad31deee78e9
Nataliia <nataliia.somka@customertimes.com>
2014-12-08 12:45:27 +0200

    added images

--- app.js
+++ app.js
@@ -43,9 +43,9 @@
         color: '#CCC',
         faces: 14,
         images: [
+            "images/gem-01.gif",
             "images/gem-02.gif",
-            "images/gem-05.gif",
-            "images/gem-09.gif"
+            "images/gem-03.gif"
         ],
         reviews: [{
             stars: 5,
@@ -68,9 +68,9 @@
             color: '#EEE',
             faces: 12,
             images: [
-                "images/gem-01.gif",
-                "images/gem-03.gif",
-                "images/gem-04.gif"
+                "images/gem-04.gif",
+                "images/gem-05.gif",
+                "images/gem-06.gif"
             ],
             reviews: [{
                 stars: 3,
@@ -92,9 +92,9 @@
             color: '#000',
             faces: 6,
             images: [
-                "images/gem-06.gif",
                 "images/gem-07.gif",
-                "images/gem-10.gif"
+                "images/gem-08.gif",
+                "images/gem-09.gif"
             ],
             reviews: [{
                 stars: 1,
